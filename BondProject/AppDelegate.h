83bb588dffa6b7d7f9b00189da9a78be842b9e26
morobororo <e115747@ie.u-ryukyu.ac.jp>
2013-11-19 00:42:31 +0900

    Initial Commit

---
+++ BondProject/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BondProject
+//
+//  Created by Miwa Oshiro on 2013/11/19.
+//  Copyright (c) 2013年 Miwa Oshiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BondProject/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  BondProject
+//
+//  Created by Miwa Oshiro on 2013/11/19.
+//  Copyright (c) 2013年 Miwa Oshiro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
